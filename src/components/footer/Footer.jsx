fcd7bc06666f122e22c9526fd1e9c04d9d3ab4a4
abdu1oh1234 <jabdulloh457@gmail.com>
2025-09-30 18:04:58 +0500

    section5 and footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,6 +6,21 @@ function Footer() {
 
   return (
     <>
+        <footer>
+      <img src={webLogo} alt="" />
+      <div className="socilas">
+        <a className="socilas-btn" href="https://x.com/?lang=ru"
+          ><i className="fa-brands fa-twitter"></i
+        ></a>
+        <a className="socilas-btn" href="https://www.facebook.com/?locale=ru_RU"
+          ><i className="fa-brands fa-square-facebook"></i
+        ></a>
+        <a className="socilas-btn" href="https://www.instagram.com/"
+          ><i className="fa-brands fa-square-instagram"></i
+        ></a>
+      </div>
+      <p>Copywright 2020 Bella Onojie.com</p>
+    </footer>
     </>
   )
 }
